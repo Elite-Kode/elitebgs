f6f39e04a42ff6e82d297b3fa8030a3981eed803
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2017-06-07 17:11:11 +0530

    Enabled all download dump routes

--- server/routes/download_dumps.js
+++ server/routes/download_dumps.js
@@ -8,23 +8,23 @@ const path = require('path');
 let router = express.Router();
 
 router.get('/body', (req, res) => {
-    // download('https://eddb.io/archive/v5/bodies.jsonl', '../dumps/bodies.jsonl', 'body')
-    //     .then(msg => {
-    //         res.json(msg);
-    //     })
-    //     .catch(err => {
-    //         res.json(err);
-    //     });
+    download('https://eddb.io/archive/v5/bodies.jsonl', '../dumps/bodies.jsonl', 'body')
+        .then(msg => {
+            res.json(msg);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 router.get('/commodity', (req, res) => {
-    // download('https://eddb.io/archive/v5/listings.csv', '../dumps/listings.csv', 'commodity')
-    //     .then(msg => {
-    //         res.json(msg);
-    //     })
-    //     .catch(err => {
-    //         res.json(err);
-    //     });
+    download('https://eddb.io/archive/v5/listings.csv', '../dumps/listings.csv', 'commodity')
+        .then(msg => {
+            res.json(msg);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 router.get('/faction', (req, res) => {
@@ -38,33 +38,33 @@ router.get('/faction', (req, res) => {
 });
 
 router.get('/station', (req, res) => {
-    // download('https://eddb.io/archive/v5/stations.json', '../dumps/stations.json', 'station')
-    //     .then(msg => {
-    //         res.json(msg);
-    //     })
-    //     .catch(err => {
-    //         res.json(err);
-    //     });
+    download('https://eddb.io/archive/v5/stations.json', '../dumps/stations.json', 'station')
+        .then(msg => {
+            res.json(msg);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 router.get('/populatedSystem', (req, res) => {
-    // download('https://eddb.io/archive/v5/systems_populated.json', '../dumps/systems_populated.json', 'populated system')
-    //     .then(msg => {
-    //         res.json(msg);
-    //     })
-    //     .catch(err => {
-    //         res.json(err);
-    //     });
+    download('https://eddb.io/archive/v5/systems_populated.json', '../dumps/systems_populated.json', 'populated system')
+        .then(msg => {
+            res.json(msg);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 router.get('/system', (req, res) => {
-    // download('https://eddb.io/archive/v5/systems.csv', '../dumps/systems.csv', 'system')
-    //     .then(msg => {
-    //         res.json(msg);
-    //     })
-    //     .catch(err => {
-    //         res.json(err);
-    //     });
+    download('https://eddb.io/archive/v5/systems.csv', '../dumps/systems.csv', 'system')
+        .then(msg => {
+            res.json(msg);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 function download(pathFrom, pathTo, type) {
