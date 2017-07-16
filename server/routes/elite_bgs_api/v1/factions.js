8e647440e30c410a53124c7167a88c221dd78d38
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2017-07-16 15:24:18 +0530

    History data is now filtered

--- server/routes/elite_bgs_api/v1/factions.js
+++ server/routes/elite_bgs_api/v1/factions.js
@@ -43,6 +43,101 @@ router.get('/', passport.authenticate('basic', { session: false }), (req, res) =
                 }
                 factions.find(query).limit(10).lean()
                     .then(result => {
+                        let states = [];
+                        let systems = [];
+                        let timemin = null;
+                        let timemax = null;
+                        if (req.query.state) {
+                            states = arrayfy(req.query.state.toLowerCase());
+                        }
+                        if (req.query.system) {
+                            systems = arrayfy(req.query.system.toLowerCase());
+                        }
+                        if (req.query.timemin && req.query.timemax) {
+                            timemin = new Date(Number(req.query.timemin));
+                            timemax = new Date(Number(req.query.timemax));
+                        }
+
+                        if (states.length === 0 && systems.length === 0 && timemin === null && timemax === null) {
+                            result.forEach((doc, index, docs) => {
+                                doc.history.sort((a, b) => {
+                                    var key1 = a.updated_at;
+                                    var key2 = b.updated_at;
+
+                                    if (key1 < key2) {
+                                        return 1;
+                                    } else if (key1 == key2) {
+                                        return 0;
+                                    } else {
+                                        return -1;
+                                    }
+                                });
+                                doc.history = [doc.history[0]];
+                                docs[index] = doc;
+                            })
+                        } else if (states.length !== 0 || systems.length !== 0) {
+                            result.forEach((doc, index, docs) => {
+                                let historySelected = []
+                                doc.history.forEach(historyDoc => {
+                                    let historyBool = false;
+                                    if (states.length > 0) {
+                                        if (states.find(eachState => {
+                                            return historyDoc.state === eachState;
+                                        })) {
+                                            historyBool = true;
+                                        }
+                                    }
+
+                                    if (systems.length > 0) {
+                                        if (systems.find(eachSystem => {
+                                            return historyDoc.system_lower === eachSystem;
+                                        })) {
+                                            historyBool = true;
+                                        }
+                                    }
+                                    if (historyBool === true) {
+                                        historySelected.push(historyDoc);
+                                    }
+                                });
+
+                                if (timemax !== null && timemin !== null) {
+                                    let historySelectedTime = [];
+                                    historySelected.forEach(history => {
+                                        if (history.updated_at > timemin && history.updated_at < timemax) {
+                                            historySelectedTime.push(history);
+                                        }
+                                    })
+                                    historySelected = historySelectedTime;
+                                } else {
+                                    historySelected.sort((a, b) => {
+                                        var key1 = a.updated_at;
+                                        var key2 = b.updated_at;
+
+                                        if (key1 < key2) {
+                                            return 1;
+                                        } else if (key1 == key2) {
+                                            return 0;
+                                        } else {
+                                            return -1;
+                                        }
+                                    });
+                                    historySelected = [historySelected[0]];
+                                }
+                                doc.history = historySelected;
+                                docs[index] = doc;
+                            })
+                        } else if (timemin !== null && timemax !== null) {
+                            result.forEach((doc, index, docs) => {
+                                let historySelected = [];
+                                doc.history.forEach(history => {
+                                    if (history.updated_at > timemin && history.updated_at < timemax) {
+                                        historySelected.push(history);
+                                    }
+                                });
+                                doc.history = historySelected;
+                                docs[index] = doc;
+                            })
+                        }
                         res.status(200).json(result);
                     })
                     .catch(err => {
@@ -58,5 +153,16 @@ router.get('/', passport.authenticate('basic', { session: false }), (req, res) =
             res.status(500).json(err);
         });
 });
+
+let arrayfy = requestParam => {
+    let regex = /\s*,\s*/;
+    let mainArray = requestParam.split(regex);
+
+    mainArray.forEach((element, index, allElements) => {
+        allElements[index] = element.toLowerCase();
+    }, this);
+
+    return mainArray;
+}
 
 module.exports = router;
