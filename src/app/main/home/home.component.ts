f86d65208d91e39159f0ed241aa5f525e1b45e1e
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2017-09-15 17:20:42 +0530

    Applying promises for my life

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 import { AuthenticationService } from '../../services/authentication.service';
 import { FactionsService } from '../../services/factions.service';
 import { SystemsService } from '../../services/systems.service';
@@ -14,6 +15,7 @@ export class HomeComponent implements OnInit {
     user: any;
     factions = [];
     systems = [];
+    monitoredSystems = [];
     constructor(
         private authenticationService: AuthenticationService,
         private factionsService: FactionsService,
@@ -44,65 +46,79 @@ export class HomeComponent implements OnInit {
             .getUser()
             .subscribe(user => {
                 this.user = user;
+                this.addSystems();
                 this.getFactions();
-                this.getAddedSystems();
             });
     }
 
     getFactions() {
         if (this.user.factions) {
+            const allFactionsGet: Promise<any>[] = [];
             this.user.factions.forEach(faction => {
-                this.factionsService
-                    .getHistory(faction.name, (Date.now() - 10 * 24 * 60 * 60 * 1000).toString(), Date.now().toString())
-                    .subscribe(factions => {
-                        factions.docs.forEach(gotFaction => {
-                            gotFaction.faction_presence.forEach(system => {
-                                this.getSystem(system.system_name);
-                            });
-                            let history: any[] = gotFaction.history;
-                            let allSystems = [];
-                            history.forEach(element => {
-                                if (allSystems.indexOf(element.system) === -1) {
-                                    allSystems.push(element.system);
-                                }
-                            });
-                            let series = [];
-                            history.sort((a, b) => {
-                                if (a.updated_at < b.updated_at) {
-                                    return -1;
-                                } else if (a.updated_at > b.updated_at) {
-                                    return 1;
-                                } else {
-                                    return 0;
-                                }
-                            });
-                            allSystems.forEach(system => {
-                                let data = [];
+                allFactionsGet.push(new Promise((resolve, reject) => {
+                    this.factionsService
+                        .getHistory(faction.name, (Date.now() - 10 * 24 * 60 * 60 * 1000).toString(), Date.now().toString())
+                        .subscribe(factions => {
+                            resolve(factions);
+                            factions.docs.forEach(gotFaction => {
+                                const history: any[] = gotFaction.history;
+                                const allSystems = [];
                                 history.forEach(element => {
-                                    if (element.system === system) {
-                                        data.push([Date.parse(element.updated_at), Number.parseFloat((element.influence * 100).toFixed(2))])
-                                    } else {
-                                        if (element.systems.findIndex(systemElement => {
-                                            return systemElement.name === system;
-                                        }) === -1) {
-                                            data.push([Date.parse(element.updated_at), null]);
-                                        }
+                                    if (allSystems.indexOf(element.system) === -1) {
+                                        allSystems.push(element.system);
                                     }
                                 });
-                                series.push({
-                                    name: system,
-                                    data: data
+                                const series = [];
+                                history.sort((a, b) => {
+                                    if (a.updated_at < b.updated_at) {
+                                        return -1;
+                                    } else if (a.updated_at > b.updated_at) {
+                                        return 1;
+                                    } else {
+                                        return 0;
+                                    }
                                 });
+                                allSystems.forEach(system => {
+                                    const data = [];
+                                    history.forEach(element => {
+                                        if (element.system === system) {
+                                            data.push([
+                                                Date.parse(element.updated_at),
+                                                Number.parseFloat((element.influence * 100).toFixed(2))
+                                            ])
+                                        } else {
+                                            if (element.systems.findIndex(systemElement => {
+                                                return systemElement.name === system;
+                                            }) === -1) {
+                                                data.push([Date.parse(element.updated_at), null]);
+                                            }
+                                        }
+                                    });
+                                    series.push({
+                                        name: system,
+                                        data: data
+                                    });
+                                });
+                                gotFaction.factionOptions = {
+                                    xAxis: { type: 'datetime' },
+                                    title: { text: 'Influence trend' },
+                                    series: series
+                                };
+                                this.factions.push(gotFaction);
                             });
-                            gotFaction.factionOptions = {
-                                xAxis: { type: 'datetime' },
-                                title: { text: 'Influence trend' },
-                                series: series
-                            };
-                            this.factions.push(gotFaction);
-                        });
-                    })
+                        },
+                        (err: HttpErrorResponse) => {
+                            reject(err);
+                        })
+                }));
             });
+            Promise.all(allFactionsGet)
+                .then(factions => {
+                    console.log(factions);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
         }
     }
 
@@ -138,9 +154,9 @@ export class HomeComponent implements OnInit {
             })
     }
 
-    getAddedSystems() {
+    addSystems() {
         this.user.systems.forEach(system => {
-            this.getSystem(system);
+            this.monitoredSystems.push(system.name);
         })
     }
 }
