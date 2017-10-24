728abf8ae6c822736821ef29108fa7d91ec32672
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2017-10-24 18:24:52 +0530

    Working on a slick about page

--- src/app/page_not_found/page-not-found.component.ts
+++ src/app/page_not_found/page-not-found.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
     templateUrl: './page-not-found.component.html'
 })
 export class PageNotFoundComponent {
+    @HostBinding('class.content-container') contentContainer = true;
     constructor() { }
 }
