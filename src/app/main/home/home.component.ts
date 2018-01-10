9510a22f742ff3156efb24f410cc796be70a35b3
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2018-01-10 22:03:56 +0530

    System urls were being called twice

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -75,7 +75,6 @@ export class HomeComponent implements OnInit {
                         });
                     });
                 });
-                this.getSystems();
             })
             .catch(err => {
                 console.log(err);
