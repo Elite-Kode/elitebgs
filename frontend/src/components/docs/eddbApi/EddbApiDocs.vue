8b7bedba7aaf54e72c144d9f46718e658694514a
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2021-04-23 23:03:08 +0530

    Removed docs for old APIs

--- frontend/src/components/docs/eddbApi/EddbApiDocs.vue
+++ frontend/src/components/docs/eddbApi/EddbApiDocs.vue
@@ -19,21 +19,21 @@ export default {
   data () {
     return {
       specs: [
-        {
-          versionName: 'V1',
-          specLocation: 'https://eddbapi.kodeblox.com/api/v1/api-docs.json',
-          swaggerLocation: 'https://eddbapi.kodeblox.com/api/v1/docs'
-        },
-        {
-          versionName: 'V2',
-          specLocation: 'https://eddbapi.kodeblox.com/api/v2/api-docs.json',
-          swaggerLocation: 'https://eddbapi.kodeblox.com/api/v2/docs'
-        },
-        {
-          versionName: 'V3',
-          specLocation: 'https://eddbapi.kodeblox.com/api/v3/api-docs.json',
-          swaggerLocation: 'https://eddbapi.kodeblox.com/api/v3/docs'
-        },
+        // {
+        //   versionName: 'V1',
+        //   specLocation: 'https://eddbapi.kodeblox.com/api/v1/api-docs.json',
+        //   swaggerLocation: 'https://eddbapi.kodeblox.com/api/v1/docs'
+        // },
+        // {
+        //   versionName: 'V2',
+        //   specLocation: 'https://eddbapi.kodeblox.com/api/v2/api-docs.json',
+        //   swaggerLocation: 'https://eddbapi.kodeblox.com/api/v2/docs'
+        // },
+        // {
+        //   versionName: 'V3',
+        //   specLocation: 'https://eddbapi.kodeblox.com/api/v3/api-docs.json',
+        //   swaggerLocation: 'https://eddbapi.kodeblox.com/api/v3/docs'
+        // },
         {
           versionName: 'V4',
           specLocation: 'https://eddbapi.kodeblox.com/api/v4/api-docs.json',
